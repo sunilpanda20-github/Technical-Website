596f72effeed7e97bf13e7f51fc539468db3dec4
sunilpanda20-github <you@example.com>
2024-08-10 07:57:14 +0530

    First Commit
    First Date : 10-08-24

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import Pagination from './components/Pagination';
+import Search from './components/Search';
+import Stories from './components/Stories';
+import "./App.css";
+
+
+const App = () => {
+  
+  return (
+    <> 
+        <Pagination />
+        <Search />
+        <Stories />
+    </>
+  );
+}
+
+export default App;
